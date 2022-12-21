dc4008c4951f6ee5013e56a692a4dda733b0af2e
GOURAV-CTRL <120794692+GOURAV-CTRL@users.noreply.github.com>
2022-12-21 18:10:46 +0530

    Add files via upload

---
+++ image.php
@@ -0,0 +1,32 @@
+<?php
+
+error_reporting(0);
+
+?>
+
+<html>
+<body>
+
+		<form action = "" method = "post" enctype = "multipart/form-data">
+				
+				<input type = "file" name = "uploadfile" value = ""/>
+				<input type = "submit" name = "submit" value = "Upload File"/>
+		
+		
+		</form>
+
+
+</body>
+</html>
+
+
+<?php
+
+	
+	
+	$filename = $_FILES["uploadfile"]["name"];
+	$tempname = $_FILES["uploadfile"]["tmp_name"];
+	$folder = "rice/".$filename;
+	
+	move_uploaded_file($tempname,$folder);
+?>
